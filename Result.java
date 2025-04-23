584bbdbea5f1e6196833c196b21d7275b9ba4ae1
Shahmashafi <shahmashafi23@gmail.com>
2025-04-23 14:40:43 +0530

    completed

---
+++ Result.java
@@ -0,0 +1,46 @@
+
+interface Student {
+    void showAcademicScore();
+}
+
+
+interface Sports {
+    void showSportsScore();
+}
+
+class Result implements Student, Sports {
+    private String name;
+    private int rollNumber;
+    private int academicScore;
+    private int sportsScore;
+    
+    public Result(String name, int rollNumber, int academicScore, int sportsScore) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.academicScore = academicScore;
+        this.sportsScore = sportsScore;
+    }
+
+    public void showAcademicScore() {
+        System.out.println("Academic Score: " + academicScore);
+    }
+
+    public void showSportsScore() {
+        System.out.println("Sports Score: " + sportsScore);
+    }
+
+    public void showResult() {
+        System.out.println("==========================");
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        showAcademicScore();
+        showSportsScore();
+        System.out.println("==========================");
+      }  
+    public static void main(String[] args) {
+    System.out.println("\n    DISPLAYING RESULT   ");
+        Result studentResult = new Result("John", 101, 85, 90);
+        studentResult.showResult();
+    }
+}
+
